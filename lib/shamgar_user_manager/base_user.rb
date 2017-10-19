da42aaac631132a16b76bb179adb6b022b662c1a
Vic Amuso <vic@parablesoft.com>
2017-10-19 07:28:37 -0400

    Added dynamic token generation

--- lib/shamgar_user_manager/base_user.rb
+++ lib/shamgar_user_manager/base_user.rb
@@ -5,7 +5,7 @@ module ShamgarUserManager
       base.class_eval do
 	validates_presence_of :first_name,:last_name,:email,:role
 	acts_as_paranoid
-	before_save :ensure_authentication_token
+	before_save :ensure_authentication_token!
 	after_destroy :change_authentication_token!
 	scope :unconfirmed, ->{where(confirmed_at: nil)}
 	scope :confirmed, ->{where.not(confirmed_at: nil)}
@@ -36,20 +36,28 @@ module ShamgarUserManager
     end
 
     def change_authentication_token!
-      update_attributes(authentication_token: generate_authentication_token)
+      update_attributes(authentication_token: generate_token(:authentication))
+    end
+
+    def method_missing(method)
+      method = method.to_s
+      return nil unless method.starts_with?("ensure") && method.ends_with?("token!")
+      token_name = /ensure_(.+)_token!/.match(method)[1]
+      ensure_token!(token_name)
     end
 
     private 
-    def ensure_authentication_token
-      self.authentication_token = generate_authentication_token if authentication_token.blank?
+    def ensure_token!(token_name)
+      field_name = "#{token_name}_token"
+      self[field_name] = generate_token(token_name.to_sym) if self[field_name].blank?
     end
 
-
-    def generate_authentication_token
+    def generate_token(token_name)
       loop do
-	token = SecureRandom.hex(64)
-	break token unless self.class.where(authentication_token: token).first
+	token = SecureRandom.hex(100)
+	break token unless self.class.where("#{token_name}_token" => token).first
       end
     end
+
   end
 end
